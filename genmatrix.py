c07ab1a2634eef326c2a8331f6ad1b14781fe800
Jesse Wyatt <wyattjrw@gmail.com>
2018-11-02 20:25:40 +0800

    Update genmatrix.py

--- genmatrix.py
+++ genmatrix.py
@@ -1,9 +1,12 @@
 import random
 
 n = 1000
-density = 0.1
+density = 0.01
+paths = ["1000_001_A", "1000_001_B"]
 
-for i in range(1, n+1):
-    for j in range(1, n+1):
-        if (random.random() < density):
-            print("{} {} {}".format(i, j, random.uniform(-10, 10)))
+for path in paths:
+    with open(path, "w") as f:
+        for i in range(1, n+1):
+            for j in range(1, n+1):
+                if (random.random() < density):
+                    f.write("{} {} {}\n".format(i, j, random.uniform(-10, 10)))
